5a245656d957f643e60ea0eb97b01dfd6d73634f
Eunwon Song <plmqazoknijb@naver.com>
2021-07-01 07:15:14 +0000

    PHP 실습

---
+++ 03_freshman.php
@@ -0,0 +1,21 @@
+<?php
+$title = "연관배열 : Associated Array";
+include("./header.php");
+
+$freshman = array(
+		"David" => "Computer",
+		"Alice" => "Math",
+		"Elsa" => "Physics",
+		"Bob" => "Music",
+		"Chris" => array("Electronics", "Design")
+);
+print($freshman['David']);
+$freshman['Bob'] = "Movie";
+$freshman['IU'] = "Singer";
+unset($freshman["Elsa"]);
+print("<pre class=\"bg-warning text-white\">");
+print_r($freshman);
+print("</pre>");
+
+include("./footer.php");
+?>
---
+++ myphp.php
@@ -0,0 +1,22 @@
+<?php
+include('showerror.php');
+?>
+
+HTML영역
+print("Hello");
+<?php
+print("Hello");
+if(false){
+	print("Hello\n");
+	print("Hello");
+	print("Hello\n");
+}
+
+$x=3;
+$x="무야호";
+$value =1000000000;
+echo $value."<br>";
+$value= $value + 1;
+echo $value."<br>"
+
+?>
